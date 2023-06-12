cf43f8db4235069987b0f9e459072d5ab7bcbba8
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-06-12 22:11:55 +0200

    refactor: more detailed log

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { brightBlue, brightGreen, gray, italic, strikethrough, white } from 'https://deno.land/std@0.191.0/fmt/colors.ts'
+import { brightBlue, brightGreen, brightRed, gray, strikethrough, underline, white } from 'https://deno.land/std@0.191.0/fmt/colors.ts'
 import { walk } from 'https://deno.land/std@0.191.0/fs/walk.ts'
 import { valid } from 'https://deno.land/std@0.191.0/semver/mod.ts'
 import regex from 'https://esm.sh/url-regex@5.0.0'
@@ -7,14 +7,23 @@ import denoland_std from './registries/deno.land_std.ts'
 import denoland_x from './registries/deno.land_x.ts'
 import esm_sh from './registries/esm.sh.ts'
 import github from './registries/raw.githubusercontent.com.ts'
-import { Upgrade } from './createMarkdown.ts'
+
+type Upgrade = {
+  registry: string
+  package: string
+  file: string
+  fromVersion: string
+  toVersion: string
+  url: string
+  fileCount: number
+}
 
 export default async function upgrade({
   directory = Deno.cwd(),
-  extensions = ['.js', '.ts', '.json', '.md']
+  ext = ['js', 'ts', 'json', 'md']
 }: {
   directory?: string,
-  extensions?: string[]
+  ext?: string[]
 } = {}) {
   const cache = new Map<string, string>()
 
@@ -28,9 +37,11 @@ export default async function upgrade({
 
   , upgrades: Upgrade[] = []
 
+  , list: Record<string, { upgraded: number, failed: number, skipped: number, fromVersion?: string, toVersion?: string }> = {}
+
   // walk through files
   for await (const { path, isFile } of walk(directory)) {
-    if (!isFile || !extensions.some(e => path.endsWith(e)))
+    if (!isFile || !ext.some(e => path.endsWith(`.${e}`)))
       continue
 
     let content = await Deno.readTextFile(path)
@@ -42,66 +53,109 @@ export default async function upgrade({
     
     // walk through urls
     for (let url of urlMatches) {
-      url = url.replace('\'', '').replace(')', '')
+      try {
+        url = url.replace('\'', '').replace(')', '')
 
-      if (
-        url.includes('${') ||
-        registries.some(r => url === `https://${r.prefix}`)
-      )
-        continue
+        if (
+          url.includes('${') ||
+          registries.some(r => url === `https://${r.prefix}`)
+        )
+          continue
 
-      const registry = registries.filter(r => url.startsWith(`https://${r.prefix}`))[0]
+        url = url.replace('\'', '').replace(')', '')
 
-      if (!registry)
-        continue
-
-      url = url.replace('https://', '')
-
-      const name = await registry.getName(url)
-      , fromVersion = await registry.getCurrentVersion(url)
-
-      if (!valid(fromVersion))
-        continue
-
-      const toVersion = cache.get(`${registry.name}:${name}`)
-        ?? await registry.getNextVersion(name, url)
-
-      if (!cache.has(`${registry.name}:${name}`))
-        cache.set(`${registry.name}:${name}`, toVersion)
-
-      if (
-        fromVersion.replace('v', '') === toVersion.replace('v', '') ||
-        toVersion.includes('rc') ||
-        toVersion.includes('alpha') ||
-        toVersion.includes('beta') ||
-        toVersion.includes('unstable') ||
-        toVersion.includes('canary') ||
-        toVersion.includes('nightly') ||
-        toVersion.includes('dev')
-      ) {
-        console.info(gray(`${white(name)} × ${toVersion} ${italic(brightBlue('(skipped)'))}\n`))
+        if (
+          url.includes('${') ||
+          registries.some(r => url === `https://${r.prefix}`)
+        )
+          continue
   
-        continue
+        const registry = registries.filter(r => url.startsWith(`https://${r.prefix}`))[0]
+  
+        if (!registry)
+          continue
+  
+        url = url.replace('https://', '')
+  
+        const name = await registry.getName(url)
+        , fromVersion = await registry.getCurrentVersion(url)
+  
+        if (!valid(fromVersion))
+          continue
+  
+        let toVersion: string
+  
+        try {
+          toVersion = cache.get(`${registry.name}:${name}`)
+          ?? await registry.getNextVersion(name, url)
+        } catch (_) {
+          throw new Error(`ferr${JSON.stringify({ name, registry: registry.name, fromVersion })}`)
+        }
+  
+        if (!cache.has(`${registry.name}:${name}`))
+          cache.set(`${registry.name}:${name}`, toVersion)
+  
+        if (
+          fromVersion.replace('v', '') === toVersion.replace('v', '') ||
+          toVersion.includes('rc') ||
+          toVersion.includes('alpha') ||
+          toVersion.includes('beta') ||
+          toVersion.includes('unstable') ||
+          toVersion.includes('canary') ||
+          toVersion.includes('nightly') ||
+          toVersion.includes('dev')
+        ) {
+          if (list[`${registry.name}:${name}`] !== undefined)
+            list[`${registry.name}:${name}`].skipped++
+          else
+            list[`${registry.name}:${name}`] = { upgraded: 0, skipped: 1, failed: 0, fromVersion }
+    
+          continue
+        }
+  
+        // update url
+        content = content.replace(url, url.replace(fromVersion, toVersion))
+  
+        upgrades.push({
+          registry: registry.name,
+          package: await registry.getName(url),
+          fromVersion,
+          toVersion,
+          url,
+          fileCount: 1,
+          file: path
+        })
+
+        if (list[`${registry.name}:${name}`] !== undefined)
+          list[`${registry.name}:${name}`].upgraded++
+        else
+          list[`${registry.name}:${name}`] = { upgraded: 1, skipped: 0, failed: 0, fromVersion, toVersion }
+      } catch (err) {
+        if (err instanceof Error && err.message.startsWith('ferr')) {
+          const data = JSON.parse(err.message.replace('ferr', ''))
+  
+          if (list[`${data.registry}:${data.name}`] !== undefined)
+            list[`${data.registry}:${data.name}`].upgraded++
+          else
+            list[`${data.registry}:${data.name}`] = { upgraded: 0, skipped: 0, failed: 1 }
+        }
       }
-
-      // update url
-      content = content.replace(url, url.replace(fromVersion, toVersion))
-
-      upgrades.push({
-        registry: registry.name,
-        package: await registry.getName(url),
-        fromVersion,
-        toVersion,
-        url,
-        fileCount: 1,
-        file: path
-      })
-
-      console.info(gray(`${white(name)} × ${strikethrough(fromVersion)} → ${brightGreen(toVersion)}\n`))
     }
 
     await Deno.writeTextFile(path, content)
   }
+
+  let logString = `${underline('upgrade')}\n\n`
+
+  for (const [key, value] of Object.entries(list)) {
+    if (value.toVersion && value.fromVersion) {
+      logString += `${white(key.split(':')[1])} × ${strikethrough(value.fromVersion)} → ${brightGreen(value.toVersion)} (${brightGreen(`${value.upgraded} ✓`)}, ${brightBlue(`${value.skipped} ⁄`)}, ${brightRed(`${value.failed} ✗`)} failed)\n`
+    } else {
+      logString += `${white(key.split(':')[1])} × ${value.fromVersion ? value.fromVersion : '?.?.?'} (${brightGreen(`${value.upgraded} ✓`)}, ${brightBlue(`${value.skipped} ⁄`)}, ${brightRed(`${value.failed} ✗`)})\n`
+    }
+  }
+
+  console.info(gray(logString))
 
   return { upgrades }
 }
--- workflow.ts
+++
@@ -1,8 +0,0 @@
-import { createMarkdown } from './createMarkdown.ts'
-import upgrade from './mod.ts'
-
-const { upgrades } = await upgrade()
-
-, markdown = await createMarkdown(upgrades)
-
-await Deno.writeTextFile('./upgrade.md', markdown)
