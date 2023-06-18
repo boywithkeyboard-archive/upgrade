e5f9131de7528b910b5e0fed4ae71da17eee2e1c
Samuel Kopp <62482066+boywithkeyboard@users.noreply.github.com>
2023-06-18 15:41:21 +0200

    feat: emit file and dependency count

--- upgrade.ts
+++ upgrade.ts
@@ -1,16 +1,27 @@
 import { Change } from "./Change.d.ts";
 
 export async function upgrade(changes: Change[]) {
+  const files: string[] = [];
+  const dependencies: string[] = [];
+
   for (const change of changes) {
     if (change.type !== "upgrade") {
       continue;
     }
 
-    const content = await Deno.readTextFile(change.filePath);
+    if (!files.includes(change.filePath)) {
+      files.push(change.filePath);
+    }
+
+    if (!dependencies.includes(change.moduleName)) {
+      dependencies.push(change.moduleName);
+    }
 
     if (!change.currentVersion || !change.nextVersion) {
       continue;
     }
+
+    const content = await Deno.readTextFile(change.filePath);
 
     await Deno.writeTextFile(
       change.filePath,
@@ -20,4 +31,15 @@ export async function upgrade(changes: Change[]) {
       ),
     );
   }
+
+  return {
+    /**
+     * Amount of updated files.
+     */
+    files: files.length,
+    /**
+     * Amount of updated dependencies.
+     */
+    dependencies: dependencies.length,
+  };
 }
