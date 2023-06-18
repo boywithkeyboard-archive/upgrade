d4985cd931294c06b615798fe216a8b8118533e9
Tyler Mueller <muelltyl@gmail.com>
2023-06-18 09:58:02 -0500

    changes comments in Registry to JSDocs this is so that the comments show up in the editor.

--- Registry.ts
+++ Registry.ts
@@ -1,17 +1,17 @@
 export class Registry {
   public name;
   public prefix;
-  // parse package name from url (used by createUpdates())
+  /** parse package name from url (used by createUpdates()) */
   public getName;
-  // parse current version from url (used by createUpdates())
+  /** parse current version from url (used by createUpdates()) */
   public getCurrentVersion;
-  // parse next version from url (used by createUpdates())
+  /** parse next version from url (used by createUpdates()) */
   public getNextVersion;
-  // create current version url (used by createMarkdown())
+  /** create current version url (used by createMarkdown()) */
   public getCurrentVersionUrl;
-  // create next version url (used by createMarkdown())
+  /** create next version url (used by createMarkdown()) */
   public getNextVersionUrl;
-  // get github repository (used by createMarkdown())
+  /** get github repository (used by createMarkdown()) */
   public getRepository;
 
   constructor(opts: {
