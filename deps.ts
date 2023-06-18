8f27c3711bc352d023ea52c52534063a51ac7ba3
Tyler Mueller <muelltyl@gmail.com>
2023-06-18 09:56:14 -0500

    downgrades std version to 0.191.0 for semver prerelease is deprecated on 0.192.0 so we're using an older version

--- deps.ts
+++ deps.ts
@@ -3,4 +3,4 @@ export * as colors from "https://deno.land/std@0.192.0/fmt/colors.ts";
 export * as semver from "https://deno.land/std@0.191.0/semver/mod.ts#pin";
 export * as fs from "https://deno.land/std@0.192.0/fs/walk.ts";
 export { Checkbox } from "https://deno.land/x/cliffy@v0.25.7/prompt/checkbox.ts";
-export { gte, prerelease } from "https://deno.land/std@0.192.0/semver/mod.ts";
+export { gte, prerelease } from "https://deno.land/std@0.191.0/semver/mod.ts";
